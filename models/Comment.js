d5e4b1f4c078a1a0b057b8277d1b8542b7735580
Innei <tukon479@gmail.com>
2019-09-01 22:02:24 +0800

    09/01 增加评论model

---
+++ models/Comment.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  // comment id
+  cid: {
+    type: Number,
+    default: 1
+  },
+  // post id
+  pid: {
+    type: Number
+  },
+  content: {
+    type: String
+  },
+  create_time: {
+    type: String
+  },
+  author: {
+    type: String
+  },
+  author_id: {
+    type: Number
+  },
+  owner_id: {
+    type: Number
+  },
+  url: {
+    type: String
+  },
+  parent: {
+    type: Number
+  }
+})
+
+module.exports = mongoose.model('Comment', schema)
