1a76c436a986bfca01a1acd2a096d82fb52c7f98
Innei <tukon479@gmail.com>
2019-09-03 21:38:13 +0800

    09/03 搜索接口实现

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -16,13 +16,112 @@ const router = express.Router({ mergeParams: true })
 router.get('/', auth(), async (req, res) => {
   const page = req.query.page || 1
   const size = req.query.size || 10
-  const model = await Post.find({ author: req.username })
-    .skip((page - 1) * size)
-    .limit(size)
-    .sort({ modifyTime: 1 })
-  const totalPage = Math.ceil(
-    (await User.findOne({ username: req.username })).options.publish_nums / size
-  )
+  const keyword = req.query.keyword || ''
+  const state = Number(req.query.state) || -1
+
+  // 文章状态 1 => 已发布 2 => 已过期 -1 => 未指定
+  let status = {}
+
+  switch (state) {
+    case 1:
+      status = {
+        $and: [
+          {
+            $or: [
+              {
+                outdateTime: {
+                  $gt: Date.now()
+                }
+              },
+              { isOutdate: false }
+            ]
+          },
+          {
+            limitTime: { $ne: 0 }
+          }
+        ]
+      }
+      break
+    case 2:
+      status = {
+        $or: [
+          {
+            $and: [
+              {
+                outdateTime: {
+                  $lt: Date.now()
+                }
+              },
+              { isOutdate: true }
+            ]
+          },
+          { limitTime: 0 }
+        ]
+      }
+      break
+    default:
+      break
+  }
+  /* const status =
+    state === 2
+      ? {
+          $or: [
+            {
+              $and: [
+                {
+                  outdateTime: {
+                    $lt: Date.now()
+                  }
+                },
+                { isOutdate: true }
+              ]
+            },
+            { limitTime: 0 }
+          ]
+        }
+      : {
+          $and: [
+            {
+              $or: [
+                {
+                  outdateTime: {
+                    $gt: Date.now()
+                  }
+                },
+                { isOutdate: false }
+              ]
+            },
+            {
+              limitTime: { $ne: 0 }
+            }
+          ]
+        } */
+  const model = (keyword || state !== -1)
+    ? await Post.find({
+        author: req.username,
+        $or: [
+          {
+            title: new RegExp(keyword, 'ig')
+          },
+          { content: new RegExp(keyword, 'ig') }
+        ],
+        ...status
+      }).sort({ modifyTime: 1 })
+    : await Post.find({
+        author: req.username,
+        // ...status
+      })
+        .skip((page - 1) * size)
+        .limit(size)
+        .sort({ modifyTime: 1 })
+
+  const totalPage = (keyword || state !== -1)
+    ? // ? Math.ceil(model.length / size)
+      1
+    : Math.ceil(
+        (await User.findOne({ username: req.username })).options.publish_nums /
+          size
+      )
   const currentPage = Number(page)
   res.send({
     options: {
